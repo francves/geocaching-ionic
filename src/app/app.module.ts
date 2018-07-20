7928089882c075f1e92c9b01ce881bbbd0e3d981
Francisco Velásquez <francves1711@gmail.com>
2018-07-20 13:29:44 -0400

    Agregando servicios de acceso a datos con firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,22 @@ import { HomePage } from '../pages/home/home';
 import { PerfilPage } from '../pages/perfil/perfil';
 import { TabsPage } from '../pages/tabs/tabs';
 import { LugarPage } from '../pages/lugar/lugar';
+import { AboutPage } from '../pages/about/about';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { LugaresProvider } from '../providers/lugares/lugares';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBhCugolV85M5LzK4m84ticqZJetIRdzbU",
+    authDomain: "ionicbasic-cc5bb.firebaseapp.com",
+    databaseURL: "https://ionicbasic-cc5bb.firebaseio.com",
+    projectId: "ionicbasic-cc5bb",
+    storageBucket: "ionicbasic-cc5bb.appspot.com",
+    messagingSenderId: "674783617199"
+};
+
 
 @NgModule({
   declarations: [
@@ -16,11 +32,15 @@ import { LugarPage } from '../pages/lugar/lugar';
     HomePage,
     PerfilPage,
     TabsPage,
-    LugarPage
+    LugarPage,
+    AboutPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -28,12 +48,14 @@ import { LugarPage } from '../pages/lugar/lugar';
     HomePage,
     PerfilPage,
     TabsPage,
-    LugarPage
+    LugarPage,
+    AboutPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    LugaresProvider
   ]
 })
 export class AppModule {}
---
+++ src/providers/lugares/lugares.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import {AngularFireDatabase} from 'angularfire2/database';
+
+/*
+  Generated class for the LugaresProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class LugaresProvider {
+
+  constructor(public http: HttpClient, public afDB: AngularFireDatabase) {
+    console.log('Hello LugaresProvider Provider');
+  }
+  public getLugares(){
+  	return this.afDB.list('/lugares/');
+  }
+
+  public getLugar(id){
+  	return this.afDB.object('/lugares/' + id);
+  }
+
+  public createLugar(lugar){
+  	return this.afDB.database.ref('/lugares/' + lugar.id).set(lugar);
+  }
+
+  public editLugar(lugar){
+  	return this.afDB.database.ref('/lugares/' + lugar.id).set(lugar);
+  }
+
+  public deleteLugar(lugar){
+	return this.afDB.database.ref('/lugares/' + lugar.id).remove(lugar);
+  }
+
+}
